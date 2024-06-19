bd07d456413fe8b262a607da4a1381048e2a8eb8
GongoraLeo <gongora.leo@gmail.com>
2024-06-19 22:46:48 +0200

    plantilla index menu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Personal-web')
+
+@section('blog')
+
+    <article class="blog">
+        <h2>Título</h2>
+        <p>Author: </p>
+    </article>
+    <article class="blog">
+        <h2>Título</h2>
+        <p>Author: </p>
+    </article>
+    <article class="blog">
+        <h2>Título</h2>
+        <p>Author: </p>
+    </article>
+    <article class="blog">
+        <h2>Título</h2>
+        <p>Author: </p>
+    </article>
+
+@endsection
+
+@section('blog')
+
+    <article class="portfolio">
+        <h2>Project</h2>
+        <p>Technologies</p>
+    </article>
+    <article class="portfolio">
+        <h2>Project</h2>
+        <p>Technologies</p>
+    </article>
+    <article class="portfolio">
+        <h2>Project</h2>
+        <p>Technologies</p>
+    </article>
+    <article clasportfolio">
+        <h2>Project</h2>
+        <p>Technologies</p>
+    </article>
+
+@endsection
